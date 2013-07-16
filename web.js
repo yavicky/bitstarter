8c9a778daf0641eb7832ac62c4107f1131b09e79
陳雅婷 <Vicky@chintadashitingdeiMac.local>
2013-07-16 23:17:34 +0800

    For HW4-1

--- web.js
+++ web.js
@@ -1,15 +1,17 @@
 var express = require('express');
 
+var htmlfile = 'index.html';
+
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html');
+  var content = fs.readFileSync(htmlfile);
   response.send(content.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
